abbbda5f515c489c0dd6ef0ec3786cc86e28ca59
HongYeonLee <llhongyan@naver.com>
2026-03-22 21:56:13 +0900

    props drilling 없애기

--- chapter-2/src/components/ButtonGroup.tsx
+++ chapter-2/src/components/ButtonGroup.tsx
@@ -1,21 +1,15 @@
+import { useCount } from '../context/CounterProvider';
 import Button from './Button';
 
-interface ButtonGroupProps {
-    handleIncrement?: () => void;
-    handleDecrement?: () => void;
-}
+const ButtonGroup = () => {
+    const { handleIncrement, handleDecrement } = useCount();
 
-const ButtonGroup = ({
-    handleIncrement,
-    handleDecrement,
-}: ButtonGroupProps) => {
     return (
         <div>
-            {/*Button 함수 호출, 매개변수로 onClick 함수와 string 변수를 props에 담아 전달*/}
-            <Button onClick={handleIncrement} text="+1 증가"/>
-            <Button onClick={handleDecrement} text="-1 감소"/>
+            <Button onClick={handleIncrement} text='+1 증가'></Button>
+            <Button onClick={handleDecrement} text='-1 감소'></Button>
         </div>
     );
 };
 
-export default ButtonGroup
+export default ButtonGroup;
