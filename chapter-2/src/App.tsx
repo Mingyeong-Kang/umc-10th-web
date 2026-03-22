fac9553ed9670675d7462778ba982eb6a263d646
HongYeonLee <llhongyan@naver.com>
2026-03-22 21:52:43 +0900

    커스텀 훅 사용하기

--- chapter-2/src/App.tsx
+++ chapter-2/src/App.tsx
@@ -2,17 +2,22 @@ import './App.css'
 import {useContext, useState} from 'react';
 import ButtonGroup from './components/ButtonGroup';
 import { CounterContext } from './context/CounterProvider';
+import { useCount } from './context/CounterProvider';
 
 function App() {
-  const context = useContext(CounterContext);
+  // const context = useContext(CounterContext);
+  const context = useCount();
+
   console.log(context);
 
   return (
     <>
       <h1>{context?.count}</h1>
       <ButtonGroup
-        handleIncrement={context?.handleIncrement}
-        handleDecrement={context?.handleDecrement}
+        //?를 지워도 더이상 타입스크립트에서 에러가 발생하지 않음
+        // 우리가 useCount 훅에서, context가 없는 경우의 에러처리를 했기 때문 
+        handleIncrement={context.handleIncrement}
+        handleDecrement={context.handleDecrement}
         >
       </ButtonGroup>
     </>
