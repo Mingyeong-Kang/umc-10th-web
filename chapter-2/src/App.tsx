c484de333144f3bb32874ba67711f3e3e647878b
HongYeonLee <llhongyan@naver.com>
2026-03-22 19:45:26 +0900

    카운트 1 증가, 1 감소 기능 구현하기

--- chapter-2/src/App.tsx
+++ chapter-2/src/App.tsx
@@ -2,38 +2,23 @@ import './App.css'
 import {useState} from 'react';
 
 function App() {
-  //초기 상태: name, age, nickname, city를 가진 객체
-  const [person, setPerson] = useState({
-    name: '이홍연',
-    age: 24,
-    nickname: '여니',
-    city: '', //city 키를 미리 넣어둬야 타입이 추론됨
-  });
+  const [count, setCount] = useState<number>(0);
 
-  //city update
-  const updateCity = () => {
-    setPerson((prevPerson) => ({
-      ...prevPerson, //기존 상태 복사
-      city: '서울' //city 값만 덮어쓰기
-    }));
+  //카운트 1씩 증가
+  const increaseCount = () => {
+    setCount(count + 1);
   };
 
+  //카운트 1씩 감소
+  const decreaseCount = () => {
+    setCount(count - 1);
+  }
 
-  //age 1 증가
-  const increaseAge = () => {
-    setPerson((prevPerson) => ({
-      ...prevPerson, //기존 상태 복사
-      age: prevPerson.age + 1, //age만 + 1
-    }));
-  };
   return (
     <>
-    <h1>이름: {person.name}</h1>
-    <h2>나이: {person.age}</h2>
-    <h3>닉네임: {person.nickname}</h3>
-    {person.city && <h4>도시: {person.city}</h4>}
-    <button onClick={updateCity}>도시 추가</button>
-    <button onClick={increaseAge}>나이 증가</button>
+    <h1>카운트: {count}</h1>
+    <button onClick={increaseCount}>카운트 1 증가</button>
+    <button onClick={decreaseCount}>카운트 1 감소</button>
     </>
   );
 }
