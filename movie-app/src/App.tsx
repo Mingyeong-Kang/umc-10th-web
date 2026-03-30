76c958fe1c093bef43b589cce877a2a763823691
Aileen050925 <Aileen050925@gmail.com>
2026-03-31 04:45:18 +0900

    week2 mission2

--- movie-app/src/App.tsx
+++ movie-app/src/App.tsx
@@ -1,6 +1,29 @@
+import HomePage from "./pages/Homepage";
 import MoviePage from "./pages/Moviepage";
-const App = () => {
-  return <MoviePage />;
-};
+import NotFoundPage from "./pages/NotFoundpage";
+import MovieDetailPage from "./pages/MovieDetailPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "movies/:category",
+        element: <MoviePage />,
+      },
+      {
+        path: "movie/:movieId",
+        element: <MovieDetailPage />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
 
 export default App;
