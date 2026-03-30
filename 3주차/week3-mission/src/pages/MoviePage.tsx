cfafa1d6759d388afcd281727c3c056863b01bfb
HongYeonLee <llhongyan@naver.com>
2026-03-30 16:49:48 +0200

    미션2: LoadingSpinner 구현하기

--- 3주차/week3-mission/src/pages/MoviePage.tsx
+++ 3주차/week3-mission/src/pages/MoviePage.tsx
@@ -2,13 +2,20 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import type { Movie, MovieResponse } from "../types/movie";
 import MovieCard from "../components/MovieCard";
+import { LoadingSpinner } from "../components/LoadingSpinner";
 
 export default function MoviePage() {
     const [movies, setMovies] = useState<Movie[]>([]);
-
+    //1. 로딩 상태
+    const [isPending, setIsPending] = useState(false);
+    //2. 에러 상태
+    const [isError, setIsError] = useState(false);
+    
     useEffect(() => {
         const fetchMovies = async () => {
-            const {data} = await axios.get<MovieResponse>(
+            setIsPending(true);
+            try{
+                const {data} = await axios.get<MovieResponse>(
                 'https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1', 
                 {
                     headers: {
@@ -16,13 +23,20 @@ export default function MoviePage() {
                     },
                 }
             );
-    
-            setMovies(data.results)
+            setMovies(data.results);
+            } catch {
+                setIsError(true);
+            } finally {
+                setIsPending(false);
+            }
         };
 
         fetchMovies();
     }, []);
 
+    if (isPending) {
+        return <LoadingSpinner></LoadingSpinner>;
+    }
 
     return (
         <div className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6'>
