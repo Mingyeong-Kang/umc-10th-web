bd2162b0ae81351ba515abcb29d8551b241d1cfe
HongYeonLee <llhongyan@naver.com>
2026-03-30 14:33:30 +0900

    useEffect 심화 실습

--- 3주차/useEffect_Practice/src/App.tsx
+++ 3주차/useEffect_Practice/src/App.tsx
@@ -1,23 +1,11 @@
-import { useEffect, useState } from 'react'
-import MoviesPage from './components/MoviesPage';
 import './App.css'
+import UseEffectError from './useEffect/UseEffectError';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    console.log('컴포넌트가 처음 마운트될 때 실행됩니다');
-  }, []);
-
-  useEffect(() => {
-    console.log('count 값이 ${count}로 변경될 때마다 실행됩니다');
-  }, [count]);
 
   return (
     <>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>+1 증가</button>
-      <MoviesPage></MoviesPage>
+      <UseEffectError></UseEffectError>
     </>
   );
 }
