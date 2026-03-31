1009e237179d890364f24809a6ad09c29f3da81c
jjihye-0108 <wjswlgp9511@gmail.com>
2026-03-31 15:13:15 +0900

    mission2 구현 완료

--- mission1/src/pages/not-found.tsx
+++ mission1/src/pages/not-found.tsx
@@ -1,6 +1,11 @@
 // src/pages/not-found.tsx
 const NotFound = () => {
-  return <h1>404 페이지를 찾을 수 없습니다.</h1>;
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl text-red-500">404</h1>
+      <p className="text-lg">페이지를 찾을 수 없습니다!</p>
+    </div>
+  );
 };
 
 export default NotFound;
