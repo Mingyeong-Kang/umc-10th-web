f9d9510e29b180e5dd3d482bd978a75cbb3b27e3
HongYeonLee <llhongyan@naver.com>
2026-03-31 10:42:06 +0200

    미션3: useParams를 활용한 영화 상세 페이지 개발

--- 3주차/week3-mission/src/types/movie.ts
+++ 3주차/week3-mission/src/types/movie.ts
@@ -22,3 +22,89 @@ export type MovieResponse = {
     total_results: number;
 
 };
+export interface Genre {
+    id: number;
+    name: string;
+}
+
+export interface ProductionCompany {
+    id:number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+}
+
+export interface ProductionCountry {
+    iso_3166_1: string;
+    name: string;
+}
+
+export interface SpokenLanguages {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+}
+
+export type MovieDetail = {
+    adult: boolean;
+    backdrop_path: string;
+    belongs_to_collection: boolean | null;
+    budget: number;
+    genres: Genre[];
+    homepage: string;
+    id: number;
+    imdb_id: string;
+    origin_country: string[];
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    production_companies: ProductionCompany[];
+    production_countries: ProductionCountry[];
+    release_date: string;
+    revenue: number;
+    runtime: number;
+    spoken_languages: SpokenLanguages[];
+    status: string;
+    tagline: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
+
+export type Cast = {
+    adult: boolean;
+    gender: number;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string;
+    cast_id: number;
+    character: string;
+    credit_id: string;
+    order: number;
+}
+
+export type Crew = {
+    adult: boolean;
+    gender: number;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string;
+    credit_id: number;
+    department: string;
+    job: string;
+}
+
+export type MovieCredit = {
+    id: number;
+    cast: Cast[];
+    crew: Crew[];
+}
