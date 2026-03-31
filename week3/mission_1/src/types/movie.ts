43bd3ea7f6e0985860f2b07f475a3537843d2e85
이수빈 <sarasoobin1019@gmail.com>
2026-03-31 21:25:28 +0900

    3주차 미션3

--- week3/mission_1/src/types/movie.ts
+++ week3/mission_1/src/types/movie.ts
@@ -20,5 +20,60 @@ export type MovieResponse= {
   results:Movie[];
   totalPages: number;
   total_results: number;
+}
 
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface ProductionCompany {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+export interface MovieDetails {
+  id: number;
+  title: string;
+  original_title: string;
+  overview: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+  release_date: string;
+  runtime: number;
+  vote_average: number;
+  vote_count: number;
+  genres: Genre[];
+  tagline: string;
+  status: string;
+  budget: number;
+  revenue: number;
+  production_companies: ProductionCompany[];
+  original_language: string;
+  popularity: number;
+  adult: boolean;
+}
+
+export interface Cast {
+  id: number;
+  name: string;
+  character: string;
+  profile_path: string | null;
+  order: number;
+}
+
+export interface Crew {
+  id: number;
+  name: string;
+  job: string;
+  department: string;
+  profile_path: string | null;
+}
+
+export interface Credits {
+  id: number;
+  cast: Cast[];
+  crew: Crew[];
 }
