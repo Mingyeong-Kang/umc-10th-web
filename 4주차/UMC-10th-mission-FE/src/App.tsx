b7056e815a2c59d17532012f461e26694ab16203
HongYeonLee <llhongyan@naver.com>
2026-04-06 01:45:52 +0900

    4주차 미션2: 프로젝트 기본 세팅

---
+++ 4주차/UMC-10th-mission-FE/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+//1. 홈페이지
+//2. 로그인 페이지
+//3. 회원가입 페이지
+
+function App() {
+  return (
+    <>
+    <h1 className={"text-2xl text-blue-500"}>홍연</h1>
+    </>
+  )
+}
+
+export default App
